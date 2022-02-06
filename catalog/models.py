abd5daeabab7f39ecdab65821ae20f235f1c3c3e
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2022-02-05 22:41:33 -0300

    Modelos criados

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class ReadingMaterial(models.Model):
+    original_title = models.CharField(max_length=200)
+    adapted_title = models.CharField(max_length=200)
+    first_publication_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=200)
+    coutry_of_origin = models.CharField(max_length=200)
+    synopsis = models.TextField()
