41463a499ab8cf9fa04164b665d475669de28248
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2022-02-14 17:29:55 -0300

    base.html criada os outros templates extende dela, modificacoes nas urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,30 @@
+import unicodedata
+
 from django.views import generic
-from .models import ReadingMaterial
+from .models import ReadingMaterial, Gender
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+
+_genders = {}
+for gen in Gender.objects.all():
+    key = unicodedata.normalize('NFD', gen.gender_name)
+    key = ''.join(c for c in key if not unicodedata.combining(c)) 
+    _genders.setdefault(key.lower().replace(' ', '+'), gen.gender_name.upper())
+
 
 class IndexView(generic.ListView):
     template_name = 'catalog/index.html'
-    paginate_by = 2
+    context_object_name = 'genders'
+
+    def get_queryset(self):
+        return _genders
+
+
+class AllbooksView(generic.ListView):
+    template_name = 'catalog/allbooks.html'
     model = ReadingMaterial
+    paginate_by = 2
 
 
 class DetailView(generic.DetailView):
@@ -14,3 +34,23 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+
+def ResultView(request, gender):
+    books = []
+
+    for o in ReadingMaterial.objects.all():
+        for g in o.gender_set.all():
+            gen = unicodedata.normalize('NFD', g.gender_name)
+            gen = ''.join(c for c in gen if not unicodedata.combining(c))
+            if gender.replace('+', ' ') == gen:
+                books.append(o)
+
+    paginator = Paginator(books, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/result.html', {'gender': _genders[gender], 'page_obj': page_obj})
+    
+
