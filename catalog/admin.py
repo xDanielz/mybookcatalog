c7c66464f1108c40585a371f26dd045f40c54745
Daniel da Silva Rodrigues <Dan.sil.rod.20@gmail.com>
2022-02-16 11:35:28 -0300

    campo de imagem adicionado no modelo readingmaterial e admin.py modificado para que este campo apareça

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,17 +4,21 @@ from .models import ReadingMaterial, Gender
 
 
 class GenderInline(admin.TabularInline):
+    
     model = Gender
     extra = 1
 
 
 class ReadingMaterialAdmin(admin.ModelAdmin):
+
     fieldsets = [
         ('Title',            {'fields': ['original_title', 'adapted_title']}),
         ('Date information', {'fields': ['pub_date', 'year_of_publication'], 'classes': ['collapse']}),
         ('Author',           {'fields': ['author', 'coutry_of_origin']}),
-        ('About',            {'fields': ['synopsis', 'additional_information', 'number']})
+        ('About',            {'fields': ['synopsis', 'additional_information', 'number']}),
+        ('Cover',             {'fields': ['image']})
     ]
+
     inlines = [GenderInline]
     list_display = ('original_title', 'adapted_title', 'pub_date')
     list_filter = ['pub_date', 'author', ]
